7bf86f43820dc0bb87069a0715acbb276fb54b98
ohziiilov3 <klbaskerville0520@gmail.com>
2022-10-15 17:30:35 -0700

    get route and post route working

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,17 +1,33 @@
 const express = require('express')
 const router = express.Router()
+const Subscriber = require("../models/subscriber");
 
 // Getting all 
-router.get('/', (req, res )=> {
-res.send('Hello World')
+router.get('/', async (req, res ) => {
+try {
+    const subscribers = await Subscriber.find()
+    res.json(subscribers)
+} catch (error) {
+    res.status(500).json({message: error.message})
+}
 });
-// Get One 
-router.get('/:id',(req,res)=>{
-    req.params.id
-})
-// Create One 
-router.post('/', (req, res) => {
 
+// Get One 
+router.get('/:id', getSubscriber, (req, res) => {
+  res.json(res.subscriber);
+});
+// Create One 
+router.post('/',  async (req, res) => {
+    const subscriber = new Subscriber({
+        name: req.body.name,
+        subscriberToChannel: req.body.subscriberToChannel
+    })
+    try {
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
 });
 // Updating One
 
@@ -20,13 +36,25 @@ router.patch('/:id', (req, res) => {
 });
 // Deleting One 
 router.delete('/:id', (req, res) => {
-
+    
 });
 
 
 
 
-
+async function getSubscriber(req,res,next) {
+    let subscriber 
+    try {
+        subscriber = await Subscriber.findById(req.params.id)
+        if(subscriber == null){
+            return res.status(404).json({message: 'Cannot find Subscriber'})
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message})
+    }
+    res.subscriber = subscriber
+    next()
+}
 
 
 module.exports = router;
